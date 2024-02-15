8938fa5aa5918acb79a7df5295109bf023a8e9bc
Yuri Krukovskiy <ykrukovskiy@gmail.com>
2024-02-15 18:52:20 +0300

    add task2, edit task1

---
+++ Lesson1/Task1/A_more_than_B.cs
@@ -0,0 +1,8 @@
+﻿int a = 10;
+int b = 100;
+if (a > b){
+    Console.WriteLine("Число a больше b");
+}
+else{
+    Console.WriteLine("Число b больше a");
+}
---
+++ Lesson1/task2/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 10;
+int b = 1000;
+int c = 5000;
+
+if (a > b){
+    if (a > c)
+    Console.WriteLine("Максимальное число " + a);
+}
+else{
+if (b > c)
+    Console.WriteLine("Максимальное число " + b);
+    else 
+    Console.WriteLine("Максимальное число " + c);
+}
+
