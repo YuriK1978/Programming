636aa60dba1efd23ab332173f477d328923beb1c
Yuri Krukovskiy <ykrukovskiy@gmail.com>
2024-03-24 20:38:09 +0300

    F_string.py modified

--- Python/F_string.py
+++ Python/F_string.py
@@ -1,18 +1,42 @@
-# No1
-text = 'Python'
-print(f'{text}')
-print(f'{text:%<20}')
-print(f'{text:_>20}')
-print(f'{text:.^20}') #print("{:*^20s}".format("Sammy"))
+# ................No1 Positions
+#text = 'Python'
+#print(f'{text}')
+#print(f'{text:%<20}')
+#print(f'{text:_>20}')
+#print(f'{text:.^20}') 
+#print("{:*^20s}".format("Sammy"))
 
-# No2
-new_open_string = "Sammy loves {} {}."
-print(new_open_string.format("open-source", "software"))
 
-# No3
-print("Sammy the {0} {1} a {pr}.".format("shark", "made", pr = "pull request"))
+# ................No2
+#new_open_string = "Sammy loves {} {}."
+#print(new_open_string.format("open-source", "software"))
 
-# No4 обратные номера
-print("Sammy is a {3}, {2}, and {1} {0}!".format("happy", "smiling", "blue", "shark"))
 
+# ................No3
+#print("Sammy the {0} {1} a {pr}.".format("shark", "made", pr = "pull request"))
+#Output:  Sammy the shark made a pull request.
+
+
+# ................No4 обратные номера
+#print("Sammy is a {3}, {2}, and {1} {0}!".format("happy", "smiling", "blue", "shark"))
 #Output: Sammy is a shark, blue, and smiling happy!
+
+
+# ................No5 Float format
+#a = 22.2222
+#print(f'{a:.2f}')
+#print(round(a, 2))
+#Output: 22.22
+
+
+# ................No6 Date/time
+#from datetime import datetime
+#now: datetime = datetime.now()
+#print(f'{now:%d.%m.%y (%H.%M.%S)}')
+#Output:24.03.24 (20.24.03)
+
+
+#................No7 Calculations
+#championship: str = 'Fakel is the champion'
+#print(f'{championship = }')
+
